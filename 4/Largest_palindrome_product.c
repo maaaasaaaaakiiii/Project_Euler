0ce01178b69020e45177c55302947219b7ba0c77
吉田真基 <yoshida@nile2014.local>
2017-10-01 14:47:11 +0900

    4

--- 4/Largest_palindrome_product.c
+++ 4/Largest_palindrome_product.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 
 int main(){
-  int palidromic, cnt = 0;
+  int i, j, k, max_i, max_j, palidromic, max_palidromic = 0, cnt;
   int remem[100];
 
-  scanf("%d", &palidromic);
-
-  while(palidromic != 0){
-    remem[cnt] = palidromic % 10;
-    palidromic = palidromic / 10;
-    cnt++;
+  for(i = 100; i <= 999; i++){
+    cnt = 0;
+    for(j = 100; j <= 999; j++){
+      palidromic = i * j;
+      while(palidromic != 0){
+	remem[cnt++] = palidromic % 10;
+	palidromic = palidromic / 10;
+      }
+      cnt--;
+      for(k = 0; k <= cnt; k++){
+	if(remem[k] != remem[cnt--])
+	  break;
+      }
+      if(k > cnt && max_palidromic < i*j){
+	max_palidromic = i*j;
+	max_i = i;
+	max_j = j;
+      }
+      cnt = 0;
+    }
   }
+
+  printf("%d %d %d\n", max_i, max_j, max_palidromic);
+
+  return 0;
+}
   
